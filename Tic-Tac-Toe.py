d98b97a46f835adae47133aa6882e64def3d8c77
Abbhinav <abbhinav99mishra@gmail.com>
2024-03-27 15:25:21 +0530

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -12,37 +12,59 @@ titleLabel.pack()
 frame2 = Frame(root)
 frame2.pack()
 
+turn = "x"
+
+def play(event):
+    global turn
+    button = event.widget
+
+    if turn == "x":
+        button["text"]="X"
+        turn = "o"
+
+    else:
+        button["text"]= "O"
+        turn="x"
 #Tic Tac Toe board
 
 #first row
-button1 = Button(frame2, text = " ", width = 16, height = 8)
+button1 = Button(frame2, text = " ", width = 4, height = 2, font=("Arial", 30), bg = "gray", relief= RAISED, borderwidth=5)
 button1.grid(row=0, column=0)
+button1.bind("<Button-1>", play)
 
-button1 = Button(frame2, text = " ", width = 16, height = 8)
+button1 = Button(frame2, text = " ", width = 4, height = 2, font=("Arial", 30), bg = "gray", relief= RAISED, borderwidth=5)
 button1.grid(row=0, column=1)
+button1.bind("<Button-1>", play)
 
-button1 = Button(frame2, text = " ", width = 16, height = 8)
+button1 = Button(frame2, text = " ", width = 4, height = 2, font=("Arial", 30), bg = "gray", relief= RAISED, borderwidth=5)
 button1.grid(row=0, column=2)
+button1.bind("<Button-1>", play)
 
 #second row
-button1 = Button(frame2, text = " ", width = 16, height = 8)
+button1 = Button(frame2, text = " ", width = 4, height = 2, font=("Arial", 30), bg = "gray", relief= RAISED, borderwidth=5)
 button1.grid(row=1, column=0)
+button1.bind("<Button-1>", play)
 
-button1 = Button(frame2, text = " ", width = 16, height = 8)
+button1 = Button(frame2, text = " ", width = 4, height = 2, font=("Arial", 30), bg = "gray", relief= RAISED, borderwidth=5)
 button1.grid(row=1, column=1)
+button1.bind("<Button-1>", play)
 
-button1 = Button(frame2, text = " ", width = 16, height = 8)
+button1 = Button(frame2, text = " ", width = 4, height = 2, font=("Arial", 30), bg = "gray", relief= RAISED, borderwidth=5)
 button1.grid(row=1, column=2)
+button1.bind("<Button-1>", play)
 
 #Third row
-button1 = Button(frame2, text = " ", width = 16, height = 8)
+button1 = Button(frame2, text = " ", width = 4, height = 2, font=("Arial", 30), bg = "gray", relief= RAISED, borderwidth=5)
 button1.grid(row=2, column=0)
+button1.bind("<Button-1>", play)
 
-button1 = Button(frame2, text = " ", width = 16, height = 8)
+button1 = Button(frame2, text = " ", width = 4, height = 2, font=("Arial", 30), bg = "gray", relief= RAISED, borderwidth=5)
 button1.grid(row=2, column=1)
+button1.bind("<Button-1>", play)
 
-button1 = Button(frame2, text = " ", width = 16, height = 8)
+button1 = Button(frame2, text = " ", width = 4, height = 2, font=("Arial", 30), bg = "gray", relief= RAISED, borderwidth=5)
 button1.grid(row=2, column=2)
+button1.bind("<Button-1>", play)
 
 
 root.mainloop() # This line starts the Tkinter event loop.
